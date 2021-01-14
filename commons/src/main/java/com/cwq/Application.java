bbeb8bc00668da05714355b14ad6270c8063d5d4
ETNJTOTG <cwq1102300125@yeah.net>
2021-01-14 14:08:06 +0800

    feat(commons):commons

---
+++ commons/src/main/java/com/cwq/Application.java
@@ -0,0 +1,16 @@
+package com.cwq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenqing.chen
+ * @time 2021/01/14 14:03
+ * @description
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
